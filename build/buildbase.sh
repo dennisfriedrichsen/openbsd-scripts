de2dff4178c7072f0e067ed37a453255e0f6fab1
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2022-05-15 14:45:34 -0500

    buildbase.sh initial checkin

---
+++ build/buildbase.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+LOG_FILE=~/logs/buildworld_$(date +"%FT%T").log
+
+cd /usr/src
+date >> $LOG_FILE
+nohup doas time make obj >> $LOG_FILE
+nohup doas time make build >> $LOG_FILE
+date >> $LOG_FILE
